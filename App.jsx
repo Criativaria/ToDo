aaa222720f88370a7dbe7afa872708d63e084b00
gustmnds <37991797+gustmnds@users.noreply.github.com>
2024-03-08 22:10:37 -0300

    fix: ajustando build do apk via eas

--- App.jsx
+++ App.jsx
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import AppLoading from 'expo-app-loading';
-import { useFonts, GochiHand_400Regular } from '@expo-google-fonts/gochi-hand';
+import React from 'react';
 import Home from './pages/Home';
 import { NavigationContainer } from '@react-navigation/native';
-import Routes from './routes';
 
 export default () => {
-  let [fontsLoaded] = useFonts({
-    GochiHand_400Regular,
-  });
-
-  let fontSize = 24;
-  let paddingVertical = 6;
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <NavigationContainer>
-        <Home />
-      </NavigationContainer>
-    );
-  }
-};
+  return (
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
+  );
+}
